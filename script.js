a60f8cb87e0c0f243f146be2950d93757881ad75
Viswabaddala16 <122138862+Viswabaddala16@users.noreply.github.com>
2023-10-08 07:46:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+if (localStorage.getItem('savedUsername') && localStorage.getItem('savedPassword')) {
+            const existingButton = document.createElement('button');
+            existingButton.id = 'existing';
+            existingButton.textContent = 'Login as existing user';
+
+            existingButton.addEventListener('click', () => {
+                const savedUsername = localStorage.getItem('savedUsername');
+                alert(`Logged in as ${savedUsername}`);
+            });
+
+            document.body.appendChild(existingButton);
+        }
+
+        // Form submit event
+        document.getElementById('loginForm').addEventListener('submit', (e) => {
+            e.preventDefault();
+            const username = document.getElementById('username').value;
+            const password = document.getElementById('password').value;
+            const remember = document.getElementById('remember').checked;
+
+            if (remember) {
+                localStorage.setItem('savedUsername', username);
+                localStorage.setItem('savedPassword', password);
+            } else {
+                localStorage.removeItem('savedUsername');
+                localStorage.removeItem('savedPassword');
+            }
+
+            alert(`Logged in as ${username}`);
+        });
